ad71be459d3061d5336e6b35e88dab9856a7824b
reapersremorse <reapersremorse@gmail.com>
2020-09-17 16:09:37 -0400

    stuff again
    stuff again

---
+++ src/main/java/com/reapersremorse/ssa/objects/blocks/Block_Test.java
@@ -0,0 +1,13 @@
+package com.reapersremorse.ssa.objects.blocks;
+
+
+import net.minecraft.block.Block;
+import net.minecraft.block.GlassBlock;
+import net.minecraft.block.OreBlock;
+import net.minecraft.item.Item;
+
+public class Block_Test extends GlassBlock{
+    public Block_Test(Properties properties) {
+        super(properties);
+    }
+}
